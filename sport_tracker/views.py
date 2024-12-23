345483ed4ff77a4f2ea365c01bd36cdb2817572b
mutabazi-00 <mutajean30@gmail.com>
2024-12-23 16:03:19 +0200

    Update the views.
    The minus sign in front of date_added sorts the results
    in reverse order, which will display the most recent entries first.

--- sport_tracker/views.py
+++ sport_tracker/views.py
@@ -8,6 +8,13 @@ def index(request):
 
 def sports(request):
     """Show all sports."""
-    topics = Sport.objects.order_by('date_added')
+    sports = Sport.objects.order_by('date_added')
     context = {'sports': sports}
     return render(request, 'sport_tracker/sports.html', context)
+
+def sport(request, topic_id):
+    """Show a single sport and all its entries."""
+    Sport = Sport.objects.get(id=topic_id)
+    entries = sport.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'sport_tracker/sport.html', context)
