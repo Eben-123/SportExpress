b6164cfeb987c9fb5a6c4241716f65b365c84d77
mutabazi-00 <mutajean30@gmail.com>
2024-12-23 10:48:06 +0200

    Update views to include sports page.
    - The sports() function will receive one parameter i.e. the request
    object Django received from the server.
    - We query the database by asking for the Sport objects,
    sorted by the date_added attribute. Then we store the resulting queryset
    in sports.
    - Finally we define a context that we'll send to the template.

--- sport_tracker/views.py
+++ sport_tracker/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from .models import Sport
+
 def index(request):
     """The home page for SportExpress."""
     return render(request, 'sport_tracker/index.html')
+
+def sports(request):
+    """Show all sports."""
+    topics = Sport.objects.order_by('date_added')
+    context = {'sports': sports}
+    return render(request, 'sport_tracker/sports.html', context)
