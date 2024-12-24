3d04ab77eaaff7b3db06fca1bc9ccb136e47063b
mutabazi-00 <mutajean30@gmail.com>
2024-12-24 15:41:33 +0200

    Create a form.
    - We first import the forms module and model to work with, i.e. Sport.
    - We define a class SportForm, which inherits from forms.ModelForm
    - We built a form from the Sport model and include only the text field.
    - Finally we tell Django not to generate a label for the text field.

---
+++ sport_tracker/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+from .models import Sport
+
+class SportForm(forms.ModelForm):
+    class Meta:
+        model = Sport
+        fields = ['text']
+        labels = {'text': ''}
