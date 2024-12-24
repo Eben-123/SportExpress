f07fdb18fdd06dcaa3038a8797e6d847eade634a
mutabazi-00 <mutajean30@gmail.com>
2024-12-24 17:27:00 +0200

    Create new_topic() view function.
    - Update the import statement to include the SportForm.
    - We import the function redirect, which we'll use to redirect
    the user back to the sports page after they submit their topic.

--- sport_tracker/views.py
+++ sport_tracker/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Sport
+from .forms import SportForm
 
 def index(request):
     """The home page for SportExpress."""
@@ -18,3 +19,19 @@ def sport(request, sport_id):
     entries = sport.entry_set.order_by('-date_added')
     context = {'sport': sport, 'entries': entries}
     return render(request, 'sport_tracker/sport.html', context)
+
+def new_sport(request):
+    """Add a new sport."""
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = SportForm()
+    else:
+        # POST data submitted; process data.
+        form = SportForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('sport_tracker:topics')
+    
+    # Display a blank or invalid form.
+    context = {'form': form}
+    return render(request, 'sport_tracker/new_sport.html', context)
