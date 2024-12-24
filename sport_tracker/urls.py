e24adfeb3d9cf9fc0a273d136363c7ca0563aaa8
mutabazi-00 <mutajean30@gmail.com>
2024-12-24 15:48:03 +0200

    Create the URL for new_sport.
    This URL pattern sends requests to the view function new_topic().
    When the user wants to add a new topic, they'll be directed to the
    appropriate page /new_topic/

--- sport_tracker/urls.py
+++ sport_tracker/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('sports/', views.sports, name='sports'),
     # Detail page for a single topic.
     path('sports/<int:sport_id>/', views.sport, name='sport'),
+    # Page for adding a new sport
+    path('new_sport/', views.new_sport, name='new_sport'),
 ]
