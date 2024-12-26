045005d0006cdb0756e0dc63ec676771521836d0
mutabazi-00 <mutajean30@gmail.com>
2024-12-26 10:05:07 +0200

    Create Entry ModelForm.
    - We made a new class called EntryForm that inherits from forms.ModelForm
    - The EntryForm class contains a nested Meta class which lists the model
    it is based on and the field to include in the form.
    - We also included the widgets attribute; using a forms.Textarea element,
    we customized the input widget for the field 'text' so that the
    text area will be 80 columns wide instead of the default 40.

--- sport_tracker/forms.py
+++ sport_tracker/forms.py
@@ -1,9 +1,16 @@
 from django import forms
 
-from .models import Sport
+from .models import Sport, Entry
 
 class SportForm(forms.ModelForm):
     class Meta:
         model = Sport
         fields = ['text']
         labels = {'text': ''}
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ['text']
+        labels = {'text': ' '}
+        widgets = {'text': forms.Textarea(attrs={'cols': 80})}
