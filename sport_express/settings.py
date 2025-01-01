59978761b9e7aa82eab4a814a412930c942e58ed
mutabazi-00 <mutajean30@gmail.com>
2025-01-01 16:45:28 +0200

    Set bootstrap4 in settings.
    For Django to have access to bootstrap4, we had to include in the
    settings.

--- sport_express/settings.py
+++ sport_express/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
     'sport_tracker',
     'users',
 
+    # Third party apps.
+    'bootstrap4',
+
     # Default django apps.
     'django.contrib.admin',
     'django.contrib.auth',
