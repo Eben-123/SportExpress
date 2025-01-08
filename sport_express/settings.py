42b47e29d43808c38ed655b28012e9ee0858b033
mutabazi-00 <mutajean30@gmail.com>
2025-01-08 21:24:36 +0200

    Allow multiple hosts.

--- sport_express/settings.py
+++ sport_express/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&6ukbx4=@-s4g^iu&^s6=dy!$rz2r07e$q=+0@993lds#@=#lt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sportexpress.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
